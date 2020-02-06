483ec577ebd89c242d52e1d7fa5b73197ece7026
Robert Gravina <robert.gravina@gmail.com>
2020-02-06 21:16:05 +0900

    Initial Commit

---
+++ RetroRampage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RetroRampage
+//
+//  Created by Robert Gravina on 2020/02/06.
+//  Copyright © 2020 Robert Gravina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
