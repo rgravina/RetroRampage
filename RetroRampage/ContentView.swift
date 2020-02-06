8efc0531d6e6e54cb5f8a93283fbaec6e6fd0138
Robert Gravina <robert.gravina@gmail.com>
2020-02-06 22:02:18 +0900

    Add Color type.

--- RetroRampage/ContentView.swift
+++ RetroRampage/ContentView.swift
@@ -5,7 +5,22 @@ struct ContentView: View {
 
     var body: some View {
         Image(uiImage: image)
-            .background(Color.black)
+            .background(SwiftUI.Color.black)
             .aspectRatio(contentMode: .fit)
     }
+}
+
+public struct Color {
+    var r, g, b: UInt8
+    var a: UInt8 = 255
+}
+
+public extension Color {
+    static let clear = Color(r: 0, g: 0, b: 0, a: 0)
+    static let black = Color(r: 0, g: 0, b: 0)
+    static let white = Color(r: 255, g: 255, b: 255)
+    static let gray = Color(r: 192, g: 192, b: 192)
+    static let red = Color(r: 255, g: 0, b: 0)
+    static let green = Color(r: 0, g: 255, b: 0)
+    static let blue = Color(r: 0, g: 0, b: 255)
 }
